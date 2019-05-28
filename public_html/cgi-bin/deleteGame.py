2d2d7b1a514aa4362681473fce7b7d318412659d
Paulette Tawil <ptawil@mail.yu.edu>
2019-05-28 11:05:11 -0400

    added rest of files

---
+++ public_html/cgi-bin/deleteGame.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3                                                                            
+
+import mysql.connector, cgi
+print("Content-type:text/html\r\n\r\n")
+cnx = mysql.connector.connect(user='ptawil', password='ptawil', database='ptawil', host='local\
+host')                                                                                         
+print('<html>')
+print('<head>')                                                                                
+print('<title>View Game Info</title>')                                                         
+print('</head>')
+print('<body>')
+cursor = cnx.cursor()
+form = cgi.FieldStorage()
+game_id = form.getvalue('game_id')
+
+query = "DELETE FROM player_game_stats WHERE game_id = %s"
+val = (game_id,)
+cursor.execute(query,val)
+
+query = "DELETE FROM game WHERE game_id = %s"
+cursor.execute(query, val)
+
+cnx.commit()
+
+print('<p> Game ' + game_id + ' was deleted. </p>')
+print('<a href="viewOrDeleteGames.py"> See updated list of games. </a>')
+print('</body>')
+print('</html>')
+
---
+++ public_html/cgi-bin/updatePlayerInfo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3                                                                            
+
+import mysql.connector, cgi
+print("Content-type:text/html\r\n\r\n")
+cnx = mysql.connector.connect(user='ptawil', password='ptawil', database='ptawil', host='localhost')                                                                                        
+print('<html>')
+print('<head>')
+print('<title>Update Player Info</title>')                                                   
+print('</head>')
+print('<body>')
+cursor = cnx.cursor()
+form = cgi.FieldStorage()
+player_id = form.getvalue('player_id')
+name = form.getvalue('name')
+position = form.getvalue('position')
+team = form.getvalue('team')
+team_id = team.split(":")[0]
+query = "UPDATE player SET name = %s, position= %s, team_id = %s WHERE player_id = %s"
+vals = (name, position, team_id, player_id)
+cursor.execute(query, vals)
+cnx.commit()
+print('<p> Player ' + player_id + ' was updated. </p>')
+print('<a href="deleteOrViewPlayer.py?team=' + team + '&submitted=Submit"> See update </a>')
+print('</body>')
+print('</html>')
